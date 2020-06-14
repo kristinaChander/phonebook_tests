20c10024c3be9ce2950eedaee496f12e625d97bc
kristinaChander <christina-wow@mail.ru>
2020-06-14 21:54:58 +0300

    final variant

--- src/main/java/services/PropertyUtil.java
+++ src/main/java/services/PropertyUtil.java
@@ -12,7 +12,7 @@ public final class PropertyUtil {
     public static final Properties TEST_PROPERTIES = getProperties("test.properties");
 
     @SneakyThrows
-    private static Properties getProperties(String propertyFilePath){
+    private static Properties getProperties(String propertyFilePath) {
         Properties properties = new Properties();
         properties.load(services.PropertyUtil.class.getClassLoader().getResourceAsStream(propertyFilePath));
         return properties;
